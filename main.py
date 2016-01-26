5e6dee1a50f09ffd06c49c610b17510a55d19ef4
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-01-26 16:57:26 -0500

    just some cleaning up.  need to add https option to argparse

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from find_all_emails import GetEmails
 
 def main(args):
     email_scraper = GetEmails(args.domain)
-    
     if not email_scraper.is_valid_domain(args.domain):
         print "Not a valid domain name."
         sys.exit(1)
-        
     email_scraper.find_emails_in_site()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
