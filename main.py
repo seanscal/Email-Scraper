f5c2487a21e253cf88578cc4a1fa25da8c9a9943
Scal <seanscala@gmail.com>
2017-04-26 21:14:49 -0400

    broken but heres a json

--- main.py
+++ main.py
@@ -12,13 +12,53 @@ from bs4 import BeautifulSoup
 import urllib2
 import lxml.html
 import collections
+import json
 
-def is_not_image(self, link):
-    '''
-    Determines whether a given href is not an image
-    '''
-    image_tags = [".png", ".jpeg", ".jpg", ".gif"]
-    return all([tag not in link for tag in image_tags])
+def get_links(soup):
+
+    linkDictionary = {}
+    
+    data = soup.findAll('div', attrs={'class':'title location truncate'})
+    for div in data:
+        links = div.findAll('a')
+        for a in links:
+            linkDictionary[a.contents[0]] = a['href']
+
+    linkDictionary = collections.OrderedDict(sorted(linkDictionary.items()))
+
+    return linkDictionary
+
+def get_phone_numbers(key, soup):
+    phoneDictionary = {}
+
+    #phone numbers
+    data = soup.find('span', attrs={'itemprop':'telephone'})
+    if data:
+        phoneDictionary[key] = data.contents[0]
+    else:
+        phoneDictionary[key] = "None Found"
+    return phoneDictionary
+
+def get_delivery_zones(key, soup):
+    deliveryDictionary = {}
+
+    #delivery zones
+    data = soup.find('div', attrs={'id':'delivery-modal'})
+    if data:
+        body = data.find('div', attrs={'class':'modal-body'})
+        contents = []
+        
+        if body.contents:
+            for content in body.contents:
+                stringToAdd = str(content).lstrip().split(" - ")[0]
+                if "<br/>" not in stringToAdd and stringToAdd not in contents:
+                    contents.append(stringToAdd)
+                contents.sort()
+            deliveryDictionary[key] = contents
+        else:
+            deliveryDictionary[key] = ["shouldnt be here"]
+    else: 
+        deliveryDictionary[key] = []
 
 
 def main(args):
@@ -31,13 +71,9 @@ def main(args):
     text = urllib2.urlopen(url).read()
     soup = BeautifulSoup(text, "lxml")
 
-    data = soup.findAll('div', attrs={'class':'title location truncate'})
-    for div in data:
-        links = div.findAll('a')
-        for a in links:
-            linkDictionary[a.contents[0]] = a['href']
+    linkDictionary = get_links(soup)
 
-    linkDictionary = collections.OrderedDict(sorted(linkDictionary.items()))
+    jsonArray = []
 
     for key, value in linkDictionary.iteritems():
         key.rstrip()
@@ -45,48 +81,19 @@ def main(args):
         text = urllib2.urlopen(url).read()
         soup = BeautifulSoup(text, "lxml")
 
-        #phone numbers
-        data = soup.find('span', attrs={'itemprop':'telephone'})
-        if data:
-            phoneDictionary[key] = data.contents[0]
-        else:
-            phoneDictionary[key] = "None Found"
+        phoneDictionary = get_phone_numbers(key, soup)
+        deliveryDictionary = get_delivery_zones(key, soup)
 
-        #delivery zones
-        data = soup.find('div', attrs={'id':'delivery-modal'})
-        if data:
-            body = data.find('div', attrs={'class':'modal-body'})
-            contents = []
-            
-            if body.contents:
-                for content in body.contents:
-                    stringToAdd = str(content).lstrip().split(" - ")[0]
-                    if "<br/>" not in stringToAdd and stringToAdd not in contents:
-                        contents.append(stringToAdd)
-                    contents.sort()
-                deliveryDictionary[key] = contents
-            else:
-                deliveryDictionary[key] = ["shouldnt be here"]
+        data = {"name": key, 
+                "phone": phoneDictionary[key], 
+                "delivery_zones": deliveryDictionary[key]}
+        jsonArray.append(data)
 
-        else: 
-            deliveryDictionary[key] = []
+    data = {}
+    data['caretakers'] = jsonArray
 
-        print str(key) \
-                + "\nPhone Number: " + str(phoneDictionary[key]) \
-                + "\nDelivery Zones: " + str(deliveryDictionary[key]) + "\n"
-
-
-
-
-
-    '''
-    Runs the email finder and creates the arguments to parse through.
-    '''
-    # email_scraper = GetEmails(args.domain, args.protocol)
-    # if not email_scraper.is_valid_domain(args.domain):
-    #     print "Not a valid domain name."
-    #     sys.exit(1)
-    # email_scraper.find_emails_in_site()
+    with open('caretakers.json', 'w') as f:
+        f.write(json.dumps(data, ensure_ascii=False))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
